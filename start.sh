7fea1769eb80370a82ad8d9e1cc7c96d7c45bcba
professorhost <97833404+professorhost@users.noreply.github.com>
2022-12-01 01:05:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/professorhost/RashmikaBot.git /RashmikaBot
+  git clone https://github.com/MyBotzz/RashmikaBot.git /RashmikaBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /RashmikaBot
